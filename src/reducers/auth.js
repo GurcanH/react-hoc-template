234396480c907442a51b6d05e7651b13ae9286bc
Gurcan Hamali <gurcanhamali@gmail.com>
2020-12-30 21:35:49 +1100

    72. Auth Reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,12 @@
+import { CHANGE_AUTH } from 'actions/types';
+
+const Auth = (state = false, action) => {
+  switch (action.type) {
+    case CHANGE_AUTH:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default Auth;
